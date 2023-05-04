5c53330d2b12674f0fb44bf29280869d467a24cd
jbenola <102546460+NEKOtionSite@users.noreply.github.com>
2023-05-04 16:14:27 +0800

    added a content of dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,79 @@
-@include('layouts.side-bar')
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    @include('layouts.side-bar')
+    <div class="container-fluid col-md-10 position-absolute end-0">
+        <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
+            <div class="card-header">
+                <br><h1><b>Dashboard</b></h1><br>
+            </div>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+            <div class= "p-1">
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>50</h3>
+                        <p>TASK LISTS</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-flag"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        SHOW INFO <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+                
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>100</h3>
+                        <p>PENDING TASKS</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-info"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        SHOW INFO <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>150</h3>
+                        <p>COMPLETED TASKS</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-check-circle"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        SHOW INFO <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+</div>
+<div class="table_header">
+                <h1><b>Pending Tasks</b></h1>
+                <form action="{{ url('/tasklists') }}" method="GET">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search" name="search">
+                        <button type="submit" class="btn btn-outline-secondary"><i class="fa fa-search"></i></button>
+                    </div>
+                </form>
+            </div>
+            <br>
+            <div class="card">
+                <div class="table_section">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr class="text-center">
+                                <th class="col-sm-5">Task Name</th>
+                                <th class="col-sm-3">Deadline</th>
+                                <th class="col-sm-1">Status</th>
+                                <th class="col-sm-3">Actions</th>
+                            </tr>
+                        </thead>
+                       
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <br>
+            <div class="container1">
+                PAGINATE HERE
             </div>
         </div>
     </div>
