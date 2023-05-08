3fdb3ad50d09faa5616e3bbaeb2f7377e476ed13
vcaballero <102521477+LupusNocti@users.noreply.github.com>
2023-05-08 09:00:43 +0800

    dashboard tables now responds to side menu bar when the side menu bar is hidden. and removed the dashboard in the nav bar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
     @include('layouts.side-bar')
-    <div class="container-fluid col-md-10 position-absolute end-0">
-        <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
+    <div class="container-fluid col-md-12 position-absolute end-0">
+        <div class="table-container">
+            <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
             <div class="card-header">
                 <br><h1><b>Dashboard</b></h1><br>
             </div>
